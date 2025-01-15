1ffbd748c68da31c57e3ef39d2266980d2cfd9f9
idab <thedab97@gmail.com>
2025-01-15 13:52:30 +0300

    add files

---
+++ components/Icons/Logo.tsx
@@ -0,0 +1,13 @@
+export default function Logo() {
+  return (
+    <a href="https://rgsu.by" target="_blank" rel="noopener noreferrer">
+      <img
+        src="/logo-white.svg"
+        alt="Logo"
+        width="120"
+        height="26"
+        aria-labelledby="logo-title-header"
+      />
+    </a>
+  );
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.png" />
+          <meta
+            name="description"
+            content="Географический Диктант 2024"
+          />
+          <meta property="og:site_name" content="eco-dict.vercel.app" />
+          <meta
+            property="og:description"
+            content="Географический Диктант 2024"
+          />
+          <meta property="og:title" content="Географический Диктант 2024" />
+        </Head>
+        <body className="bg-black antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
